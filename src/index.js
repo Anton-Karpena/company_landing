1258fafa0acd7bc0645d57cbb00cfc389748613b
Anton Karpena <antoniokarpena@gmail.com>
2021-12-01 10:51:04 +0200

    .

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const dropDownButton = document.getElementById('dropdown');
-const menuDropItem = document.querySelectorAll('.menu__drop-item');
+const menuDropItem = document.querySelectorAll('.menu__dropdown-item');
 
 const dropMenu = () => {
   if (menuDropItem[0].classList.contains('opened')) {
-    menuDropItem.forEach((item) => item.classList.remove('opened'))
+    menuDropItem.forEach((item) => item.classList.remove('opened'));
   } else {
     menuDropItem.forEach((item) => item.classList.add('opened'));
   }
